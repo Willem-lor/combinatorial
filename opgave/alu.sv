207a76b275c1c83e63917f52ac2a22d46bf44e32
Willem-lor <willem.lories@kuleuven.be>
2025-03-17 08:36:07 +0000

    mux2

--- opgave/alu.sv
+++ opgave/alu.sv
@@ -1,7 +1,9 @@
 // Code your design here
-module mux2 ();
+module mux2 #(parameter int N = 6) (input [N-1:0] d0, d1, input s, output[N-1:0] y);
+assign y = s ? d1 : d0;
 
 endmodule
+
 
 
 module mux4 ();
